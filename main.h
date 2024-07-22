f0b0ab090d4382c27b603de6297b9653c02aabc4
Xa-C24 <9335@holbertonstudents.com>
2024-07-22 14:24:40 +0200

    Code - Create file "main.h": include header guards and prototype for  "_printf " and "_putchar".

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef Main_H
+#define Main_H
+
+#include <stdlib.h>				/* library to use malloc - Free Malloc. */
+#include <stdarg.h>				/* library to use variadic fonctions. */
+
+
+int _printf(const char *format, ...);
+/* Function that display output according to given format. */
+
+int _putchar(char c);
+/* To write a single character to standard output (stdout). */
+
+int _strlen(const, char *);
+/* Calculate the length of a string. */
+
+
+#endif
+	

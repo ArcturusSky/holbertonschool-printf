b0449db5a4cc77b56359c77891f653a9c9f31a25
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-23 11:33:23 +0200

    Redaction - Improving the comments to be clearer
    Code -  Creating the structure "SpecifierFormat" used in the "get_specifier_function.c" file and the homonym function.
    - Adding the prototype of the function _strcmp used in the "get_specifier_function.c"

--- main.h
+++ main.h
@@ -1,14 +1,34 @@
-#ifndef Main_H
-#define Main_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdlib.h>				/* library to use malloc - Free Malloc. */
-#include <stdarg.h>				/* library to use variadic fonctions. */
+#include <stdlib.h>				/** Library that allow us to use `malloc` `Free`. */
+#include <stdarg.h>				/** Library that allow us to use variedic fonctions. */
 
+
+/**
+ * struct SpecifierFormat - Structure to set an array of specifiers
+ *
+ * @Specifier_Entry: the specifier given for the format
+ * @Printing_Function: The function associated
+ */
+
+typedef struct SpecifierFormat
+{
+	char *Specifier_Entry;
+	int (*Printing_Function)(char *);
+} FormatSpec;
 
 int _printf(const char *format, ...);
-/* Function that display output according to given format. */
+/** Function that display output according to given format. */
 
 int _putchar(char c);
-/* To write a single character to standard output (stdout). */
+/** To write a single character to standard output (stdout). */
+
+int _strcmp(char *s1, char *s2);
+/** _strcmp - Fonction that allows us to compare two strings of char */
+
+
+
+
 
 #endif

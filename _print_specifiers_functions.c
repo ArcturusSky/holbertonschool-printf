2442dc7491cbc8eb664125238c108da37998c533
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-25 21:05:16 +0200

    Fix - While satisfying Betty earlier I removed an "else" and broke everything. I changed the "while" by a for loop to win some lines and fix the function.

--- _print_specifiers_functions.c
+++ _print_specifiers_functions.c
@@ -13,7 +13,6 @@
 int print_char(char c)
 	{
 		_putchar(c);
-
 		return (0);
 	}
 /**
--- _printf.c
+++ _printf.c
@@ -2,6 +2,7 @@
 #include <unistd.h>				/** Library that allows us to use `write`function */
 #include <stdlib.h>				/** Library that allow us to use `malloc` `Free`. */
 #include <stdarg.h>				/** Library that allow us to use variedic fonctions. */
+
 /**
  * _printf - Customised function for displaying messages.
  * @format: Given string to print eache character and check if hold
@@ -9,21 +10,22 @@
  *
  * Return: final size to print
  */
+
 int _printf(const char *format, ...)
 {
-	int final_size_to_print = 0; /** Declaring a variable to store final size. */
-	va_list print_argument_list; /** Declaring argument list. */
+	int final_size_to_print = 0; /** Declaring a var to store final size. */
+	va_list print_argument_list; /** Declaring list of variable arguments. */
 
 	va_start(print_argument_list, format);
 	if (format == NULL) /** Check if the format is NULL. */
 	{
 		return (0);
 	}
-	while (*format)
+	for (; *format != 0 ; format++)
 	{
 		if (*format == '%') /** Check if a specifier is found */
-		{	/** Increment the format to read the next char */
-			format++;
+		{
+			format++; /** Increment to give see next character */
 			if (*format == 'c')
 			{
 				print_char(va_arg(print_argument_list, int));
@@ -45,8 +47,8 @@ int _printf(const char *format, ...)
 				print_integer(va_arg(print_argument_list, int));
 			}
 		}
-		write(1, format, 1); /** print a char coming */
-		format++;			/** from "format" which isn't affected by a specifier */
+		else
+		write(1, format, 1); /** print a char from sdinput to stdout n/a by spec. */
 		final_size_to_print++;
 	}
 	va_end(print_argument_list);

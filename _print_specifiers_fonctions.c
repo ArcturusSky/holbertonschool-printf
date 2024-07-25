6b10345bbe4e14a959e41d1add5a231e09746d77
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-25 17:51:07 +0200

    Fix -
    Removed the unecessary libary "<stdarg.h>"
    Changed each type of return for the three first 'print_ *" functions (fixing my own mistake)
    
    Removed the unecessary use of "va_list arg" into 'print_decimal' and 'print_integer'  and put back "int" parameters and so fixing every related issues
    
    Changed variables names to be clearer or in english
    
    Moved the declaration of variable inside 'print_decimal' and 'print_integer' functions, to the begining of the function. But not their initialization.
    
    Overall - Improved a bit comments or aligned

--- _print_specifiers_fonctions.c
+++ _print_specifiers_fonctions.c
@@ -1,132 +1,123 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdarg.h>
 
 /**
  * print_char - print character.
  * @c: The character to be printed.
  * This function takes a character as input and prints it using the `_putchar`
  * function.
+ *
+ * Return: The number of characters printed.
  */
 
-	void print_char(char c)
-		{
-			_putchar(c);
-		}
+int print_char(char c)
+	{
+		_putchar(c);
+
+		return (0);
+	}
 /**
- * print_string - print a string of characters.
+ * print_string - Prints a string of characters.
  * @string: The character to be printed.
  *
  * Return: The number of characters printed.
  */
 
-	void print_string(char *string)
-		{
-			while (*string != 0)			/*loop to null character */
-			{
-				_putchar(*string);			/* Print the current character */
-				string++;					/* Move to the next charater in the string */
-			}
-		}
-
-/**
- * print_percent - Print charater '%'.
- *
- * Description: This function prints the '%
- * character using the _putchar function.
- */
-
-	void print_percent(void)
-		{
-			_putchar('/%');     /* Print the % character */
-		}
-
-
-	/**
-	* print_decimal - Prints a decimal number.
-	* @arg: argument to print
-	*
-	* Return: Function for print a decimal number.
-	*/
-
-	int print_decimal(va_list arg)
-
+int print_string(char *string)
 	{
-			int n = va_arg(arg, int);
-			/*Retrieve the integer passed as an argument*/
-			int num = n;
-			int i = 0;
+		while (*string != 0)			/* Loop until reaching the end of the string */
+		{
+			_putchar(*string);			/** Print the current character */
+			string++;					/** Move to the next charater in the string */
+		}
 
-			if (n < 0)
-			{
-				_putchar('-');			/* print sign. */
-				n = -n;					/* Convert n to positive. */
-				num = n;				/* change num.*/
-				i++;					/* Increment the character counter. */
-			}
-			int puissance = 1;			/* Find the highest power of 10.*/
-
-			while (num / puissance >= 10)
-			{
-				puissance *= 10;		/* Multiply by 10 to find the power.*/
-			}
-
-			while (puissance > 0)		/* Print each number. */
-			{
-				_putchar((n / puissance) % 10 + '0');
-				n %= puissance;			/* Update n to remove the printed digit. */
-				puissance /= 10;		/* Reduce power to move on to the next digit. */
-				i++;					/*  Increment the character counter. */
-			}
-			return (i);
+		return (0);
 	}
 
+/**
+* print_decimal - Prints a decimal number.
+* @decimal: integer in base 10 to print
+*
+* Return: Function for print a decimal number.
+*/
 
-	/**
-	* print_integer - Prints a integer.
-	* @arg: List of variable arguments containing the integer to be printed.
-	*
-	* Return: Function for print a integer.
-	*/
-	int print_integer(va_list arg)
+int print_decimal(int decimal)
 {
-			int i = 0;
-			int n = va_arg(arg, int);
-			/*  Retrieve the integer passed as an argument.*/
-			unsigned int number;
+		int num = decimal;				/** Copy original int to avoid altering "decimal". */
+		int count_to_print = 0;			/** Counter to count the numb of char to print. */
+		int power;
 
-
-			if (n < 0)					/* Managing negative numbers. */
-			{
-				_putchar('-');			/* Print sign'-'.*/
-				number = (unsigned int)(-n);
-				/*  Convert n to positive using an unsigned variable. */
-
-				i++;					/* Increment the character counter. */
-			}
-			else
-			{
-				number = (unsigned int)n;
-				/* If n is positive, copy it directly into num.*/
-			}
-
-
-			unsigned int divisor = 1;
-			unsigned int temp = number;
-
-			while (temp / 10 > 0)
-			{
-				divisor *= 10;			/* Multiply by 10 to find the power. */
-				temp /= 10;				/* Réduire temporairement pour continuer le calcul. */
-			}
-
-			while (divisor > 0)
-			{
-				_putchar((number / divisor) % 10 + '0');
-				/* Extract and print the figure. */
-				number %= divisor;						/* Update num to remove printed digit. */
-				divisor /= 10;							/* Reduce the divisor to move to the next digit. */
-				i++;									/*  Increment the character counter.*/
-			}
-			return (i);		/* Return the total number of characters printed. */
+		if (decimal < 0)				/** Check if decimal is negativ. */
+		{
+			_putchar('-');				/** Print sign. */
+			decimal = -decimal;			/** Convert "decimal" to positive. */
+			num = decimal;				/** Update "num".*/
+			count_to_print++;			/** Increment the character counter. */
 		}
+		power = 1;						/** Find the highest power of 10.*/
+
+		while (num / power >= 10)
+		{
+			power *= 10;				/** Multiply by 10 to find the power.*/
+		}
+
+		while (power > 0)				/** Print each number. */
+		{
+			_putchar((decimal / power) % 10 + '0');
+			decimal %= power;			/** Update decimal to remove the printed digit. */
+			power /= 10;				/** Reduce power to move on to the next digit. */
+			count_to_print++;			/** Increment the character counter. */
+		}
+		return (count_to_print);
+}
+
+
+/**
+* print_integer - Prints a integer.
+* @integer: List of variable arguments containing the integer to be printed.
+*
+* Return: Function for print a integer.
+*/
+
+int print_integer(int integer)
+{
+		int count_to_print = 0;
+		unsigned int number;
+		unsigned int divisor;
+		unsigned int temp;
+
+										/** Checking if negative numbers. */
+		if (integer < 0)
+		{
+			_putchar('-');				/** Print sign'-'.*/
+			number = (unsigned int)(-integer);
+										/**  Convert integer to positive using an unsigned variable. */
+
+			count_to_print++;			/** Increment the character counter. */
+		}
+		else
+		{
+			number = (unsigned int) integer;
+										/** If integer is positive, copy it directly into num.*/
+		}
+
+
+		divisor = 1;
+		temp = number;
+
+		while (temp / 10 > 0)
+		{
+			divisor *= 10;				/** Multiply by 10 to find the power. */
+			temp /= 10;					/** Reduce temporary to continue the calcul. */
+		}
+
+		while (divisor > 0)
+		{
+			_putchar((number / divisor) % 10 + '0');
+										/** Extract and print the figure. */
+			number %= divisor;			/** Update num to remove printed digit. */
+			divisor /= 10;				/** Reduce the divisor to move to the next digit. */
+			count_to_print++;			/**  Increment the character counter.*/
+		}
+		return (count_to_print);		/** Return the total number of char printed. */
+	}

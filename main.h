63e4853a985f288e891e0e605553b4dd62db5451
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-25 17:35:49 +0200

    Fixing and finishing
    - Removed the structure "SpecifierFormat" unecessary in the end.
    - Removed the function "int _strcmp", unecessary in end.
    - Adding some precision to the "ERROR" macro's comment.

--- main.h
+++ main.h
@@ -2,28 +2,11 @@
 #define MAIN_H
 
 #include <unistd.h>				/** Library that allows us to use `write`function */
-#include <stdlib.h>				/** Library that allow us to use `malloc` `Free`. */
-#include <stdarg.h>				/** Library that allow us to use variedic fonctions. */
-
-/** Start - Macros created */
+#include <stdlib.h>				/** Library that allows us to use `malloc` `Free`. */
+#include <stdarg.h>				/** Library that allows us to use variedic fonctions. */
 
 #define ERROR (write(1, "ERROR\n", 6))
-/** Print "ERROR" followed by a new line */
-
-/** End - Macros created */
-
-/**
- * struct SpecifierFormat - Structure to set an array of specifiers
- *
- * @Specifier_Entry: the specifier given for the format
- * @Printing_Function: The function associated
- */
-
-typedef struct SpecifierFormat
-{
-	char *Specifier_Entry;
-	int (*Printing_Function)(char *);
-} FormatSpec;
+/** Defining a MACRO to print "ERROR" in (unlikely) case of NULL format to */
 
 int _printf(const char *format, ...);
 /** Function that display output according to given format. */
@@ -31,8 +14,21 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 /** To write a single character to standard output (stdout). */
 
-int _strcmp(char *s1, char *s2);
-/** _strcmp - Fonction that allows us to compare two strings of char */
+int print_char(char c);
+/** Function that print a char */
+
+int print_string(char *string);
+/** Function that prints a string*/
+
+int print_percent(void);
+/** Function that print a % */
+
+int print_decimal(int n);
+/** Function that print an integer */
+
+int print_integer(int n);
+/** Function that print an integer */
+
 
 
 #endif

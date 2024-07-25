235b49aa630e99a5d0e274e6b5b7f869badb77d6
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-25 19:54:01 +0200

    fix - satisfying Betty

--- _printf.c
+++ _printf.c
@@ -2,7 +2,6 @@
 #include <unistd.h>				/** Library that allows us to use `write`function */
 #include <stdlib.h>				/** Library that allow us to use `malloc` `Free`. */
 #include <stdarg.h>				/** Library that allow us to use variedic fonctions. */
-
 /**
  * _printf - Customised function for displaying messages.
  * @format: Given string to print eache character and check if hold
@@ -10,30 +9,20 @@
  *
  * Return: final size to print
  */
-
 int _printf(const char *format, ...)
 {
-	/** Declaring a variable to store the final output size. */
-	int final_size_to_print = 0;
-	/** Declaration of the list of variable arguments. */
-	va_list print_argument_list;
+	int final_size_to_print = 0; /** Declaring a variable to store final size. */
+	va_list print_argument_list; /** Declaring argument list. */
 
-
-	
 	va_start(print_argument_list, format);
-	/** Check if the format is NULL. */
-	if (format == NULL)
-		{
-			ERROR;
-			return (0);
-		}
-
+	if (format == NULL) /** Check if the format is NULL. */
+	{
+		return (0);
+	}
 	while (*format)
 	{
-		/** Check if a specifier is found */
-		if (*format == '%')
-		{
-			/** Increment the format to read the next character and determine which case it is */
+		if (*format == '%') /** Check if a specifier is found */
+		{	/** Increment the format to read the next char */
 			format++;
 			if (*format == 'c')
 			{
@@ -45,8 +34,7 @@ int _printf(const char *format, ...)
 			}
 			if (*format == '%')
 			{
-				/** Just printing the special character is enough */
-				_putchar('%');
+				_putchar('%'); /** Just printing the special character is enough */
 			}
 			if (*format == 'd')
 			{
@@ -57,14 +45,10 @@ int _printf(const char *format, ...)
 				print_integer(va_arg(print_argument_list, int));
 			}
 		}
-
-		else
-		/** print a character coming from "format" which isn't affected by a specifier */
-		write(1, format, 1);
-		format++;
+		write(1, format, 1); /** print a char coming */
+		format++;			/** from "format" which isn't affected by a specifier */
 		final_size_to_print++;
 	}
-
 	va_end(print_argument_list);
 	return (final_size_to_print);
 }
